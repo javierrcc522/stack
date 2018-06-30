b46a170686d5123c16c1335688707bbf24462db8
Javier Jimenez <javierrcc522@gmail.com>
2018-06-30 01:17:30 -0700

    added just to check that old components are working

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
+    //provider can only have one child subcompoenets can have as many components
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerWords="Tech Stack" />
+      </View>
     </Provider>
   );
 };
